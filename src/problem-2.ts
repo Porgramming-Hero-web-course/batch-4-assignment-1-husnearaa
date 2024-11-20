8361e3274f0d39da7e2a85408d83080bf00b590e
husneara <husnearaa99@gmail.com>
2024-11-20 11:49:29 +0600

    has solver the 2nd problem

---
+++ src/problem-2.ts
@@ -0,0 +1,14 @@
+function removeDuplicates(arr: number[]): number[] {
+    const uniqueArray: number[] = [];
+    for (const num of arr) {
+      if (!uniqueArray.includes(num)) {
+        uniqueArray.push(num);
+      }
+    }
+    return uniqueArray;
+  }
+  
+
+  console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
+
+  
